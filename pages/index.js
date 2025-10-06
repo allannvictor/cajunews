515ad491d3b7901efca0f19f058996a7c4e08342
allannvictor <allannvictor@gmail.com>
2025-10-06 13:24:19 +0000

    adiciona scripts `lint.check` e `lint.fix`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Ju, se você quer uma gracinha, dê uma risadinha.</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste identacao");
-}
-
 export default Home;
